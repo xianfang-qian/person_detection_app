cfe8b6e547a3378864a1f94d0c7f42e8b0874472
xianfang-qian <63028324+xianfang-qian@users.noreply.github.com>
2021-11-24 16:24:12 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 source /opt/intel/openvino_2021/bin/setupvars.sh
-python3 object_detection_sample_ssd.py -m person-detection-0200.xml -i person.png
+python3 object_detection_sample_ssd.py -m person-detection-0200.xml -i person.png -o /data/output-1623.png
 echo "Show files in /app"
 ls
 echo "Show files in /data/openvino_person_detection_app"
